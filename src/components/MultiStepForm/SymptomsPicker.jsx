4d31fa86536b051ef856352ea56ca2c361e16011
FelixTheCatBG <griddesign99@gmail.com>
2019-12-27 16:30:56 +0200

    AGRO-14-DiagnosePageSteps

--- src/components/MultiStepForm/SymptomsPicker.jsx
+++ src/components/MultiStepForm/SymptomsPicker.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 // import Paper from '@material-ui/core/Paper';
-// import GridItem from '../../components/Grid/GridItem';
+import GridItem from '../../components/Grid/GridItem';
 // import GridContainer from '../../components/Grid/GridContainer';
 import withStyles from "@material-ui/core/styles/withStyles";
 import symptomService from "../../services/SymptomService";
+import GridContainer from '../Grid/GridContainer';
+import Chip from '@material-ui/core/Chip';
+
 
 const useStyles = theme => ({
     bigIndicator: {
@@ -14,6 +17,9 @@ const useStyles = theme => ({
         textAlign: 'center',
         color: theme.palette.text.secondary,
         marginBottom: 20
+    },
+    selectedChips: {
+        margin: 10
     }
 });
 
@@ -29,7 +35,8 @@ export class SymptomsPicker extends Component {
     // }
 
     state = {
-        symptoms: []
+        symptoms: [],
+        pickedSymptoms: []
     }
 
     componentDidMount () {
@@ -42,26 +49,61 @@ export class SymptomsPicker extends Component {
             });
     }
 
+    handleClick = (symptom) => {
+        const newArray = this.state.symptoms.filter(s => s.id !== symptom.id).slice(0);
+
+
+        this.setState({
+            pickedSymptoms: [ ...this.state.pickedSymptoms, symptom ],
+            symptoms: newArray
+        });
+    };
+
+    handleDelete = (symptom) => {
+        const newArray = this.state.pickedSymptoms.filter(s => s.id !== symptom.id).slice(0);
+
+        this.setState({
+            pickedSymptoms: newArray,
+            symptoms: [ ...this.state.symptoms, symptom ]
+        });
+    };
+
     render () {
+        const { classes } = this.props;
+
         return (
-            <div>
-                <h1>Symptoms Picker</h1>
-                {
-                    this.state.symptoms.map(symptom => (
-                        <div>{symptom.name}</div>
-                    ))
-                }
-                {/* <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={this.saveAndContinue}
-                >
-                    Next
-                </Button> */}
-                {/* {this.state.symptoms.map(symptom => (
-                    <p>{symptom.name}</p>
-                ))} */}
-            </div>
+            <GridContainer>
+                <GridItem xs={12}>
+                    <h1>Symptoms Picker</h1>
+                </GridItem>
+                <GridItem xs={6}>
+                    {
+                        this.state.symptoms.map(symptom => (
+                            <Chip
+                                className={classes.selectedChips}
+                                label={symptom.name}
+                                clickable
+                                color="primary"
+                                onClick={() => this.handleClick(symptom)}
+                                variant="outlined"
+                            />
+                        ))
+                    }
+                </GridItem>
+                <GridItem xs={6}>
+                    {
+                        this.state.pickedSymptoms.map(symptom => (
+                            <Chip
+                                className={classes.selectedChips}
+                                label={symptom.name}
+                                onDelete={() => this.handleDelete(symptom)}
+                                color="primary"
+                                variant="outlined"
+                            />
+                        ))
+                    }
+                </GridItem>
+            </GridContainer>
         );
     }
 }
